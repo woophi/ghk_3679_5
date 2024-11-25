9bc80a6764f92e023c306543bc47e1a517e376c9
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-11-25 10:32:02 +0100

    fix

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -22,7 +22,7 @@ export const sendDataToGA = async (payload: Payload) => {
       {
         redirect: 'follow',
         method: 'POST',
-        body: JSON.stringify({ date, ...payload, variant: 'ghk_3679_1' }),
+        body: JSON.stringify({ date, ...payload, variant: 'ghk_3679_5' }),
         headers: {
           'Content-Type': 'text/plain;charset=utf-8',
         },
